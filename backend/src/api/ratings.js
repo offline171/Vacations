f1350e1c0fc21b8f1a2baa068df1d7f7bf552117
offline171 <offline.stein@gmail.com>
2025-10-11 21:46:00 -0400

    Very basic nodejs stuff

--- backend/src/api/ratings.js
+++ backend/src/api/ratings.js
@@ -1,2 +1,7 @@
 // POST /api/ratings endpoint
 // TODO: Implement rating creation endpoint
+
+const { Router } = require('express');
+const ratingsRouter = Router();
+
+module.exports = ratingsRouter;
--- backend/src/api/users.js
+++ backend/src/api/users.js
@@ -1,2 +1,7 @@
 // POST /api/users endpoint
 // TODO: Implement user creation endpoint
+
+const { Router } = require('express');
+const usersRouter = Router();
+
+module.exports = usersRouter;
