a35b7e51d40680a5ea213fbd8ae27acbccb7ca8d
offline171 <offline.stein@gmail.com>
2025-10-13 22:08:26 -0400

    More basic stuff

---
+++ backend/src/app.js
@@ -0,0 +1,14 @@
+const path = require("node:path");
+const { Pool } = require("pg");
+const express = require("express");
+const session = require("cookie-session");
+const methodOverride = require('method-override');
+const bcrypt = require("bcryptjs");
+const passport = require("passport");
+const LocalStrategy = require('passport-local').Strategy;
+
+const bookmarksRouter = require("./api/bookmarks");
+const ratingsRouter = require("./api/ratings");
+const usersRouter = require("./api/users");
+const vacationSpotRouter = require("./api/vacationSpotRouter");
+const pool = require("./services/db");
--- backend/src/services/db.js
+++ backend/src/services/db.js
@@ -1,2 +1,11 @@
 // PostgreSQL DB connection
 // TODO: Implement DB connection logic
+const { Pool } = require("pg");
+require('dotenv').config();
+const POSTGRES_USER = process.env.POSTGRES_USER;
+const POSTGRES_PASSWORD = process.env.POSTGRES_PASSWORD;
+const POSTGRES_DB = process.env.POSTGRES_DB;
+
+module.exports = new Pool({
+  connectionString: "postgresql://" + POSTGRES_USER + ":" + POSTGRES_PASSWORD + "@db:5432/" + POSTGRES_DB
+});
