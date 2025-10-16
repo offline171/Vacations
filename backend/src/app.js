3e73f27a94b295e604c8e958ff1c22753dfeeb2e
offline171 <offline.stein@gmail.com>
2025-10-15 20:02:28 -0400

    passport pt2 plus app listen

--- backend/src/app.js
+++ backend/src/app.js
@@ -45,3 +45,19 @@ passport.use(new LocalStrategy(async (username, password, done) => {
   })
 );
 
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const { rows } = await pool.query("SELECT * FROM users WHERE id = $1", [id]);
+    const user = rows[0];
+
+    done(null, user);
+  } catch (err) {
+    done(err);
+  }
+});
+
+app.listen(3002, () => console.log("app listening on port 3002!"));
