bd6a2c2ca2e26b3a7af158fb934f0f6269c37d05
offline171 <146153141+offline171@users.noreply.github.com>
2025-10-30 17:22:53 -0400

    login and logout functionality
    not sign up yet

--- backend/src/api/users.js
+++ backend/src/api/users.js
@@ -3,8 +3,14 @@
 
 const { Router } = require('express');
 const usersController = require('../controllers/usersController');
+const passport = require('passport');
 const usersRouter = Router();
 
 usersRouter.get("/", usersController.getUsers);
+usersRouter.post("/login", passport.authenticate('local', {
+    successRedirect: "/",
+    failureRedirect: "/login",
+}));
+usersRouter.get("/logout", usersController.logOut);
 
 module.exports = usersRouter;
--- backend/src/controllers/usersController.js
+++ backend/src/controllers/usersController.js
@@ -1,5 +1,15 @@
 const db = require('../services/db');
+const passport = require('passport');
 
 exports.getUsers = (req, res) => {
     res.render("users");
+}
+
+exports.logOut = (req, res) => {
+    if(req.session){
+        req.session = null;
+        res.redirect("/");
+    } else{
+        return next(error);
+    }
 }
