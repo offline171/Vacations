f64a20e7f7272000929b4910a1f899d7c0b0dee6
offline171 <offline.stein@gmail.com>
2025-11-03 22:50:33 -0500

    Add fetch ratings function
    Based on passed userId and not the one logged in so you can check someone else's ratings

--- backend/src/controllers/ratingsController.js
+++ backend/src/controllers/ratingsController.js
@@ -3,3 +3,17 @@ const db = require('../services/db');
 exports.getRatings = (req, res) => {
     res.render("ratings");
 }
+
+async function fetchRatings(userId) {
+    try{
+        const { ratings } = await pool.query("SELECT * FROM ratings WHERE user_id = $1 ORDER BY date, id", [userId]);
+        if(ratings) {
+            return ratings;
+        } else {
+            console.error("Something went wrong fetching ratings");
+        }
+    } catch (err) {
+        console.error("Error fetching ratings:", err);
+        throw err;
+    }
+}
