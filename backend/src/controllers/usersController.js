f2d0abe384665974ee623d58a8eaeec7ae122e86
offline171 <offline.stein@gmail.com>
2025-11-05 20:32:34 -0500

    Proper sign up function

--- backend/src/controllers/usersController.js
+++ backend/src/controllers/usersController.js
@@ -5,8 +5,19 @@ exports.signUpForm = (req, res) => {
     res.render("sign-up");
 }
 
-exports.signUpEnter = (req, res) => {
-   redirect("/");
+exports.signUpEnter = async (req, res) => {
+   try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    const hashedEmail = await bcrypt.hash(req.body.email, 10);
+    await pool.query(
+        "INSERT INTO USERS (username, email, password) VALUES ($1, $2, $3)",
+        [req.body.username, hashedEmail, hashedPassword]
+    );
+    res.redirect("/");
+   } catch (error) {
+    console.error("Error during user sign-up:", error);
+    next(error);
+   }
 }
 
 exports.logInForm = (req, res) => {
