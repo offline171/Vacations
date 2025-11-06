00cc0d1c1dbebf16393d58328def179abb19d8bf
offline171 <offline.stein@gmail.com>
2025-11-05 20:32:24 -0500

    Moving things around

--- backend/src/controllers/vacationsController.js
+++ backend/src/controllers/vacationsController.js
@@ -3,13 +3,3 @@ const pool = require('../services/db');
 exports.getVacations = (req, res) => {
     res.render("vacations");
 }
-
-async function fetchVacations() {
-    try{
-        const { vacations } = await pool.query("SELECT * FROM vacations");
-        return vacations;
-    } catch (err) {
-        console.error("Error fetching vacations:", err);
-        throw err;
-    }
-}
