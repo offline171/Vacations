90193046adb54805cdf707d5267825879927e4fb
offline171 <offline.stein@gmail.com>
2025-11-05 21:54:23 -0500

    Extra bookmark service

--- backend/src/services/bookmarkService.js
+++ backend/src/services/bookmarkService.js
@@ -1,2 +1,21 @@
 // BookmarkService CRUD operations
 // TODO: Implement bookmark CRUD logic
+const pool = require('../db/pool');
+
+async function fetchBookmarks(userId) {
+    try{
+        const { bookmarks } = await pool.query("SELECT * FROM bookmarks WHERE user_id = $1 ORDER BY date, id", [userId]);
+        if(bookmarks) {
+            return bookmarks;
+        } else {
+            console.error("Something went wrong fetching bookmarks");
+        }
+    } catch (err) {
+        console.error("Error fetching bookmarks:", err);
+        throw err;
+    }
+}
+
+module.exports = {
+    fetchBookmarks
+};
