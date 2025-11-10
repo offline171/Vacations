a098f477f67be75bdb4154a0ee54783c44e47f1c
offline171 <146153141+offline171@users.noreply.github.com>
2025-11-10 18:40:45 -0500

    Making insert actually compatible with the database

--- backend/src/controllers/usersController.js
+++ backend/src/controllers/usersController.js
@@ -7,11 +7,12 @@ exports.signUpForm = (req, res) => {
 
 exports.signUpEnter = async (req, res) => {
    try {
+    const currentDate = new Date();
     const hashedPassword = await bcrypt.hash(req.body.password, 10);
     const hashedEmail = await bcrypt.hash(req.body.email, 10);
     await pool.query(
-        "INSERT INTO USERS (username, email, password) VALUES ($1, $2, $3)",
-        [req.body.username, hashedEmail, hashedPassword]
+        "INSERT INTO USERS (username, email, password, created_at, updated_at) VALUES ($1, $2, $3, $4, $5)",
+        [req.body.username, hashedEmail, hashedPassword, currentDate, currentDate]
     );
     res.redirect("/");
    } catch (error) {
