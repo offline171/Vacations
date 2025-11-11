d3a110ac92e520334a9374ef6a999534e47becf1
offline171 <146153141+offline171@users.noreply.github.com>
2025-11-11 13:14:59 -0500

    Vacations post

--- backend/src/controllers/bookmarksController.js
+++ backend/src/controllers/bookmarksController.js
@@ -8,8 +8,8 @@ exports.bookmarkEnter = async (req, res) => {
    try {
     const currentDate = new Date();
     await pool.query(
-        "INSERT INTO BOOKMARKS (user_id, vacation_id, review, created_at, updated_at) VALUES ($1, $2, $3, $4, $5)",
-        [req.user.id, req.params.id, req.body.review, currentDate, currentDate]
+        "INSERT INTO BOOKMARKS (user_id, vacation_id, created_at, updated_at) VALUES ($1, $2, $3, $4)",
+        [req.user.id, req.params.id, currentDate, currentDate]
     );
     res.redirect("/");
    } catch (error) {
--- backend/src/controllers/vacationsController.js
+++ backend/src/controllers/vacationsController.js
@@ -3,3 +3,17 @@ const pool = require('../services/db');
 exports.getVacations = (req, res) => {
     res.render("vacations");
 }
+
+exports.vacationsEnter = async (req, res) => {
+   try {
+    const currentDate = new Date();
+    await pool.query(
+        "INSERT INTO VACATIONS (name, location, description, created_at, updated_at) VALUES ($1, $2, $3, $4, $5)",
+        [req.body.name, req.body.location, req.body.description, currentDate, currentDate]
+    );
+    res.redirect("/");
+   } catch (error) {
+    console.error("Error during vacation creation:", error);
+    next(error);
+   }
+}
