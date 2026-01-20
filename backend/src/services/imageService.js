4d3c4dcfd0e147eae0bbdd627667a3eace5f7715
offline171 <146153141+offline171@users.noreply.github.com>
2026-01-19 19:14:09 -0500

    Changed how findImage was found
    Also added a thing that adds .jpg if needed

--- backend/src/services/imageService.js
+++ backend/src/services/imageService.js
@@ -35,6 +35,47 @@ async function fetchImages(){
   }
 }
 
+function findImage(vacationId, images) {
+    if(!images || images.length === 0) {
+        return "https://www.w3schools.com/images/w3schools_green.jpg"; //default image
+    }
+    let start = 0;
+    let end = images.length - 1;
+    while (start <= end) {
+        let mid = Math.floor((start + end) / 2);
+        if (images[mid].vacation_id === vacationId) {
+            if(images[mid].priority === 1) {
+                return images[mid].url;
+            } else {
+                index = mid;
+                while(index >= 0 && images[index].vacation_id === vacationId) {
+                    if(images[index].priority === 1) {
+                        return checkJPG(images[index].url);
+                    }
+                    index--;
+                }
+                return checkJPG(images[mid].url);
+            }
+        } else if (images[mid].vacation_id < vacationId) {
+            start = mid + 1;
+        } else {
+            end = mid - 1;
+        }
+    }
+    return "https://www.w3schools.com/images/w3schools_green.jpg";
+};
+
+function checkJPG(imageName) {
+  const index = imageName.lastIndexOf('.');
+  if(imageName.slice(index + 1).toLowerCase() === 'jpg') {
+    return imageName;
+  } else {
+    return imageName.slice(0, index) + '.jpg';
+  }
+}
+
 module.exports = {
-    fetchVacationImages
+    fetchVacationImages,
+    fetchImages,
+    findImage
 };
